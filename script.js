4e19ecdcf3a976de9e72e5300d69e28a26b19951
RAJEEV RANJAN <62415499+Rajeev2016@users.noreply.github.com>
2023-08-26 19:30:31 +0530

    different - different color genrate

---
+++ script.js
@@ -0,0 +1,28 @@
+const randomColor = function(){
+    const hex = '0123456789ABCDEF'
+    let color = "#"
+    for (let i = 0; i < 6 ; i++){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color;
+}
+// console.log(randomColor());
+let intervalId
+const startColorChanging = function(){
+    if(!intervalId){
+    intervalId =  setInterval(changeBgColor,1000)
+
+    }
+
+    function changeBgColor(){
+    document.body.style.backgroundColor = randomColor()
+    document.getElementById("colorId").innerHTML = `${randomColor()}`
+        
+    }
+}
+const stopColorChanging = function(){
+    clearInterval(intervalId)
+    intervalId = null
+}
+document.querySelector("#start").addEventListener("click",startColorChanging)
+document.querySelector("#stop").addEventListener("click",stopColorChanging)
